4f7d2148c76e34b7ae5230f97139479a1739b6db
Thirdhuman <robert_t_orr@yahoo.com>
2019-09-06 10:12:46 -0400

    Update - state file incl.

--- Cluster_Web_Map.R
+++ Cluster_Web_Map.R
@@ -1,3 +1,5 @@
+gc;
+
 library(tidyverse)
 library(viridis)
 library(mapview)
@@ -10,6 +12,8 @@ library(rmapshaper)
 library(geojsonio)
 library(rmapshaper)
 library(rgdal)
+library(geojsonsf)
+
 
 # Best Cluster: Cluster_Final
 
@@ -26,13 +30,26 @@ Clusters_df=Clusters_df[c('FIPS', 'Final_Cluster','OZ')]
 tract_shapes=sf::st_read("~/Desktop/Welfare_Policy/Struggling_Regions/Cluster_Analyses/Shapefiles/tract_files.shp")
 tract_shapes=subset(tract_shapes, ALAND > 1)
 
-oz_info=read.csv('/Users/rorr/Desktop/Welfare_Policy/Struggling_Regions/Opportunity Zones - Shapefiles/Opportunity_Zones.csv', stringsAsFactors = F)
+LIC_tracts=read.csv('~/Desktop/Welfare_Policy/Struggling_Regions/Cluster_Analyses/Web_Map/Resources/Extra_Shapefiles/LIC Tracts/tabula-Tract-Table.csv', stringsAsFactors = F)
+LIC_tracts$Tract_ID=str_pad(LIC_tracts$Tract_ID, 11, pad = "0")
+LIC_tracts=LIC_tracts[c("Tract_ID", 'LIC')]
+
+oz_info=read.csv('~/Desktop/Welfare_Policy/Struggling_Regions/Opportunity Zones - Shapefiles/Opportunity_Zones.csv', stringsAsFactors = F)
 oz_info$GEOID10=str_pad(oz_info$GEOID10, 11, pad = "0")
 oz_info=oz_info[c('GEOID10', "STATENAME", 'COUNTYNAME','TYPE')]
 names(oz_info)[names(oz_info) == 'TYPE'] = 'OZ_type'
 
 tract_df=merge(tract_shapes,oz_info, by.x = "GEOID", by.y = "GEOID10", all.x = T, sort = F )
 tract_df=merge(tract_df,Clusters_df, by.x = "GEOID", by.y = "FIPS", all.x = T, sort = F )
+tract_df=merge(tract_df,LIC_tracts, by.x = "GEOID", by.y = "Tract_ID", all.x = T, sort = F )
+
+unique(tract_df$OZ_type)
+unique(tract_df$LIC)
+
+tract_df$LIC = ifelse(tract_df$OZ_type == 'Low-Income Community','Low-Income Community',
+			ifelse(tract_df$OZ_type == 'Undesignated Low-Income Community', 'Low-Income Community',
+					"No"))
+
 # tract_df=subset(tract_df, STATEFP != "02" & STATEFP != "15" & STATEFP != "72")
 # tract_df=subset(tract_df, STATEFP == "48")
 tract_df=subset(tract_df, STATEFP != "72")
@@ -63,7 +80,7 @@ glimpse(tract_df)
 
 tract_df$lat <- as.numeric( sub("+", "", tract_df$INTPTLAT) )
 tract_df$lng <- as.numeric( sub("+", "", tract_df$INTPTLON) )
-tract_df[is.na(tract_df)] <- '0'
+tract_df[is.na(tract_df)] <- "N/A"
 
 # tract_df$lng <- -1*as.numeric( sub("-", "", tract_df$INTPTLON) )
 # 
@@ -71,40 +88,35 @@ tract_df[is.na(tract_df)] <- '0'
 # tract_df$lng=as.numeric(tract_df$INTPTLON)
 # tract_df$lat=NULL
 # tract_df$lng=NULL
-tract_df$id <- 1:nrow(tract_df)
+# tract_df$id <- 1:nrow(tract_df)
 
 cluster_dat=tract_df 
 glimpse(cluster_dat)
-cluster_dat=within(cluster_dat,rm(INTPTLAT,INTPTLON,ind1,ind2,ind3,STATENAME,COUNTYNAME,OZ_type,Final_Cluster))
+cluster_dat=within(cluster_dat,rm(INTPTLAT,INTPTLON,ind1,ind2,ind3,ind4,ind5,STATENAME,COUNTYNAME,OZ_type,Final_Cluster))
 cluster_dat[is.na(cluster_dat)] <- "N/A"
 
 cluster_dat=as.data.frame(cluster_dat)
 cluster_dat$GEOID=as.character(cluster_dat$GEOID)
-cluster_dat_geo <- geojson_json(cluster_dat)
-
-geojson_write(input = cluster_dat_geo, file = "/Users/rorr/Desktop/Welfare_Policy/Struggling_Regions/Cluster_Analyses/Web_Map/Final_Shapefile/OZ_Tract.geojson", overwrite = TRUE)
-
 cluster_dat=within(cluster_dat,rm(geometry))
 save(cluster_dat, file =  '~/Desktop/Welfare_Policy/Struggling_Regions/Cluster_Analyses/Web_Map/cluster_df.Rdata')
 write.csv(cluster_dat, file =  '~/Desktop/Welfare_Policy/Struggling_Regions/Cluster_Analyses/Web_Map/cluster_df.csv')
-# tract_df=st_cast(tract_df, "MULTIPOLYGON") %>% st_cast("POLYGON")
 # tract_df=lwgeom::st_make_valid(tract_df)
 
 names(tract_df)
-# tract_df <- st_transform(tract_df,"+proj=longlat +init=epsg:4326")
-# tract_df=st_transform(st_sf(tract_df), "+proj=longlat +init=epsg:4326")
-# st_transform(structure(p1, proj4string = "+init=epsg:4326"), "+init=epsg:3857")
-# proj4string(tract_df) <- CRS("+proj=longlat +datum=WGS84")
 st_crs(tract_df) ="+proj=longlat +init=epsg:4326"
 glimpse(tract_df)
 
+tract_geodf=geojson_json(tract_df)
+tract_geodf$features$id <- 1:nrow(tract_geodf)
+
+inspect(tract_geodf)
 tract_df
 geojson_write(input = tract_df, file = "/Users/rorr/Desktop/Welfare_Policy/Struggling_Regions/Cluster_Analyses/Web_Map/Final_Shapefile/Cluster_Final.geojson", overwrite = TRUE)
 
 # States
-state_shapes=sf::st_read("~/Desktop/Welfare_Policy/Struggling_Regions/Cluster_Analyses/Web_Map/Resources/states.shp")
+state_shapes=sf::st_read("~/Desktop/Welfare_Policy/Struggling_Regions/Cluster_Analyses/Web_Map/Resources/states_shapfiles/states.shp")
 state_shapes
-geojson_write(input = cluster_dat_geo, file = "/Users/rorr/Desktop/Welfare_Policy/Struggling_Regions/Cluster_Analyses/Web_Map/Final_Shapefile/OZ_Tract.geojson", overwrite = TRUE)
+geojson_write(input = state_shapes, file = "/Users/rorr/Desktop/Welfare_Policy/Struggling_Regions/Cluster_Analyses/Web_Map/Final_Shapefile/state_shapes.geojson", overwrite = TRUE)
 
 
 # 
